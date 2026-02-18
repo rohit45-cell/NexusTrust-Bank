c47120e85d2912aab3dadf18b59867166d15b140
Ganesh M <144875358+rohit45-cell@users.noreply.github.com>
2026-02-18 14:43:42 +0530

    Add build script for project setup and deployment

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py createsuperuser --noinput || true
+
+python manage.py collectstatic --noinput
