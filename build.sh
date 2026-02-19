2331e7289b85fdf55f232ff998c96574178bd4a2
Ganesh M <144875358+rohit45-cell@users.noreply.github.com>
2026-02-19 13:39:34 +0530

    Remove superuser creation from build.sh
    Removed the creation of superuser from the build script.

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py makemigrations
 python manage.py migrate
-
-python manage.py createsuperuser --noinput || true
-
 python manage.py collectstatic --noinput
